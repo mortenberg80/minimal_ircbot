5c40ec1366ab9dde3b89f477c45ff7c1b636d538
Johannes Knutsen <johannes@JohannesPC.(none)>
2009-12-09 22:33:32 +0100

    added import of local_config settings

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
-HOST='irc.homelien.no'
-PORT=6667
-NICK='myNick'
-IDENT='pybot'
-REALNAME='I am'
+HOST = 'irc.homelien.no'
+PORT = 6667
+NICK = 'mynick'
+IDENT = 'pybot'
+REALNAME = 'I am'
+CHANNEL = '#mychannel'
 
-CHANNEL='#mychannel'
+try:
+    from local_config import *
+except ImportError:
+    pass
